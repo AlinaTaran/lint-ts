a77223468f3f310dcb4dcb3a05c25d30839f6d4d
Alina <aliinaattaran@gmail.com>
2022-09-22 13:21:21 +0300

    simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,33 @@ module.exports = {
     project: './tsconfig.json',
   },
   extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'airbnb-typescript', 'plugin:prettier/recommended'],
-  plugins: [],
+  plugins: ['simple-import-sort'],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
+    // Imports
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Npm packages.
+          ['^@?\\w'],
+          // Internal
+          ['^(~components|~utils)(/.*|$)'],
+          // Side effect imports
+          ['^\\u0000'],
+          // Parent imports. Put `..` last
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
   },
 };
